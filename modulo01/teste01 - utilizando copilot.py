7f369c0c653c06ea132195aa4e608fa10c1db9f3
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-05-26 12:58:18 -0300

    Fazendo teste com copiloto

---
+++ modulo01/teste01 - utilizando copilot.py
@@ -0,0 +1,5 @@
+#TODO: Criar duas vaiáveis (numero1 e numero2), e somar e imprimir o resultado
+numero1 = 10
+numero2 = 20
+soma = numero1 + numero2
+print("A soma de", numero1, " e ", numero2, "é:", soma)
