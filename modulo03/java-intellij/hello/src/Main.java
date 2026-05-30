28438abe2915e8acbc0b223416de0db63175350c
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-05-30 11:24:50 -0300

    Executando primeiro programa no Intellij

--- modulo03/java-intellij/hello/src/Main.java
+++ modulo03/java-intellij/hello/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String msg = "Programando em ";
+        System.out.println(msg + args[0]);
+        System.out.println("argumento 2: " + args[1]);
     }
 }
