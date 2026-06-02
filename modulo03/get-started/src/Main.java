d4d22597d1f6d61633a6a734c8c1277257192399
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-06-02 20:17:22 -0300

    Keywords e tipos primitivos

--- modulo03/get-started/src/Main.java
+++ modulo03/get-started/src/Main.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
-//import java.util.*; // pega tudo que tem dentro do pacote, exceto dentro de outro pacote (não recomendado)
+// import java.util.Scanner;
+// import java.util.*; // pega tudo que tem dentro do pacote, exceto dentro de outro pacote (não recomendado)
 
 public class Main {
 
-    private final String welcomeMessage = "Olá, informe o seu nome: ";
-    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome: ";
+    // private final String welcomeMessage = "Olá, informe o seu nome: ";
+    // private final static String WELCOME_MESSAGE = "Olá, informe o seu nome: ";
 
     public static void main(String[] args){
+
+        // Aula 01 - Padrões de desenvolvimento e conceitos
         // meu comentário e uma linha
         /*
         * comentário
@@ -15,16 +17,34 @@ public class Main {
         * linhas
         * */
         // Scanner scanner = new Scanner(System.in);
-        var scanner = new Scanner(System.in);
+        // var scanner = new Scanner(System.in);
         // System.out.println("Olá, informe o seu nome: ");
-        System.out.println(WELCOME_MESSAGE);
+        // System.out.println(WELCOME_MESSAGE);
         // String name = scanner.next();
-        var name = scanner.next();
-        System.out.println("Olá, informe a sua idade: ");
+        // var name = scanner.next();
+        // System.out.println("Olá, informe a sua idade: ");
         // int age = scanner.nextInt();
-        var age = scanner.nextInt();
+        // var age = scanner.nextInt();
         // System.out.println("Olá " + name + " sua idade é " + age);
-        System.out.printf("Olá %s, sua idade é %s \n", name, age);
+        // System.out.printf("Olá %s, sua idade é %s \n", name, age);
+
+        // Aula 02 - Keywords e tipos primitivos
+        byte number1 = 1;
+        short number2 = 1;
+        int number3 = 1;
+        long number4 = 1L;
+        var number5 = 1L;
+
+        float number6 = 1.0f;
+        var number7 = 1.0f;
+        double number8 = 1.0d;
+        var number9 = 1.0d;
+
+        char character1 = 'a';
+        var character2 = 'a';
+
+        boolean verify1 = true;
+        var verify2 = true;
     }
 
 }
