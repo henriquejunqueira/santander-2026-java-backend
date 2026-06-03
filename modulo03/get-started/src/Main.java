4933122e1ab28a0dac31ea8914d6f391a0056497
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-06-02 21:26:54 -0300

    Trabalhando com operadores parte 1

--- modulo03/get-started/src/Main.java
+++ modulo03/get-started/src/Main.java
@@ -1,6 +1,8 @@
 // import java.util.Scanner;
 // import java.util.*; // pega tudo que tem dentro do pacote, exceto dentro de outro pacote (não recomendado)
 
+import java.util.Scanner;
+
 public class Main {
 
     // private final String welcomeMessage = "Olá, informe o seu nome: ";
@@ -29,22 +31,60 @@ public class Main {
         // System.out.printf("Olá %s, sua idade é %s \n", name, age);
 
         // Aula 02 - Keywords e tipos primitivos
-        byte number1 = 1;
-        short number2 = 1;
-        int number3 = 1;
-        long number4 = 1L;
-        var number5 = 1L;
+        // byte number1 = 1;
+        // short number2 = 1;
+        // int number3 = 1;
+        // long number4 = 1L;
+        // var number5 = 1L;
 
-        float number6 = 1.0f;
-        var number7 = 1.0f;
-        double number8 = 1.0d;
-        var number9 = 1.0d;
+        // float number6 = 1.0f;
+        // var number7 = 1.0f;
+        // double number8 = 1.0d;
+        // var number9 = 1.0d;
 
-        char character1 = 'a';
-        var character2 = 'a';
+        // char character1 = 'a';
+        // var character2 = 'a';
 
-        boolean verify1 = true;
-        var verify2 = true;
+        // boolean verify1 = true;
+        // var verify2 = true;
+
+        // Aula 03 - Trabalhando com operadores parte 1
+        // var scanner = new Scanner(System.in);
+        // System.out.println("Quanto é 2 + 2?");
+        // var result = scanner.nextInt();
+        // var isRight = result == 4;
+        // var isWrong = result != 4;
+        // System.out.printf("O resultado é 4, você acertou? (%s)", isRight);
+        // System.out.printf("O resultado é 4, você errou? (%s)", isWrong);
+        // System.out.printf("O resultado é 4, você acertou? (%s)", !isWrong);
+
+        // System.out.println("Quantos anos você tem?");
+        // var age = scanner.nextInt();
+        // var canDrive = age > 17;
+        // var canDrive = age >= 18;
+        // var canNotDrive = age < 18;
+        // var canNotDrive = !(age < 18);
+        // System.out.println("Você é emancipado?");
+        // var isEmancipated = scanner.nextBoolean();
+        // var canNotDrive = age <= 17;
+        // var canDrive = age >= 18 || (isEmancipated && age >= 16);
+        // System.out.printf("Você pode dirigir? (%s) \n", canDrive);
+        // System.out.printf("Você não pode dirigir? (%s) \n", canNotDrive);
+        // System.out.printf("Você pode dirigir? (%s) \n", !canNotDrive);
+        // System.out.printf("Você pode dirigir? (%s) \n", canDrive);
+
+        // System.out.printf("true && true = %s \n", true && true);
+        // System.out.printf("false && false = %s \n", false && false);
+        // System.out.printf("true && false = %s \n", true && false);
+        // System.out.printf("false && true = %s \n", false && true);
+        // System.out.println("=====================================");
+        // System.out.printf("true || true = %s \n", true || true);
+        // System.out.printf("false || false = %s \n", false || false);
+        // System.out.printf("true || false = %s \n", true || false);
+        // System.out.printf("false || true = %s \n", false || true);
+        // System.out.println("=====================================");
+        // System.out.printf("!true = %s \n", !true);
+        // System.out.printf("!false = %s \n", !false);
     }
 
 }
